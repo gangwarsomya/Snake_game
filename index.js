f9f62625b5b1cec143ca904c417ec27e3532b297
gangwarsomya <gangwarsomya16@gmail.com>
2024-07-08 11:30:27 +0530

    not completed yet

---
+++ index.js
@@ -0,0 +1,4 @@
+// constants ans variables
+let inputDrn = {x:0,y:0};
+let gameOver = new Audio('./assests/game-over-arcade-6435.mp3');
+let eatSound = new Audio('./assests/snake-hissing-6092.mp3');
